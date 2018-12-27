545166780235b8f6754d6059135517ed024eaed7
drose <drose@cbs.mpg.de>
2018-12-27 15:03:29 +0100

    implemented loading keywords from yaml

--- knowviz/index.py
+++ knowviz/index.py
@@ -1,5 +1,5 @@
 """Creating and updating database indices."""
-from typing import Sequence
+from typing import Sequence, Iterable
 
 import pyparsing as pp
 
@@ -36,5 +36,27 @@ def parse_tex_file(path: str, grammar: pp.ParserElement, parse_all=True) -> list
         path = path + ".tex"
 
     results = grammar.parseFile(path, parseAll=parse_all)
+
+    return results
+
+
+def load_quantities_index(path: str) -> dict:
+    """Load index of quantities and pseudonyms from a YAML file."""
+
+    from ruamel.yaml import YAML
+    yaml = YAML()
+
+    with open(path, "r") as file:
+        quantities = dict(yaml.load(file))
+
+    return quantities
+
+
+def parse_keyword_references(path, keywords: Iterable):
+    """Load a file (e.g. model) and find references to a given set of keywords."""
+
+    keywords = list(keywords)
+    grammar = create_grammar(keywords)
+    results = list(parse_tex_file(path, grammar))
 
     return results
--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -30,3 +30,28 @@ def test_parse_tex_file(path, keywords, expected):
     path = path + ".tex"
     results = list(parse_tex_file(path, grammar))
     assert results == expected
+
+
+def test_load_quantities_index():
+
+    from knowviz.index import load_quantities_index
+
+    path = "data/metadata/quantities.yml"
+    quantities = load_quantities_index(path)
+
+    expected = dict(q1="q1", q2="q2")
+
+    assert quantities == expected
+
+
+def test_parse_keyword_reference():
+
+    from knowviz.index import load_quantities_index
+    quantities = set(load_quantities_index("data/metadata/quantities.yml"))
+
+    path = "data/models/m1.tex"
+    from knowviz.index import parse_keyword_references
+
+    results = set(parse_keyword_references(path, quantities))
+    for keyword in ["q1", "q2"]:
+        assert keyword in results
